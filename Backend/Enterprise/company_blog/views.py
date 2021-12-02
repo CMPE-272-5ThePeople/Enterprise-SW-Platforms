d639d8a02fc7384e0643f6ea30f8cc405f7316a1
Aryan Jadon <aryan.jadon@sjsu.edu>
2021-12-01 21:27:27 -0800

    added blogs

--- Backend/Enterprise/company_blog/views.py
+++ Backend/Enterprise/company_blog/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
